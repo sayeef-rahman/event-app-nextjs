05252d22b8dde2dcdc85ea396ddc687e588ab4b4
sayeef-rahman <sayeef.cse@gmail.com>
2022-10-08 20:52:36 +0600

    event structure done

---
+++ pages/events/[...slug].js
@@ -0,0 +1,7 @@
+export default function FilteredEventPage() {
+  return (
+    <div>
+      <h1>Event Slug Page</h1>
+    </div>
+  );
+}
---
+++ pages/events/[eventId].js
@@ -0,0 +1,5 @@
+export default function EventDetailsPage(){
+    return <div>
+        <h1>Single Event Details</h1>
+    </div>
+}

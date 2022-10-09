314d069e8c57a25ddc7a2a499c327f4dd4c7b444
sayeef-rahman <sayeef.cse@gmail.com>
2022-10-09 20:46:54 +0600

    button desing completed

--- components/events/SingleEvent.js
+++ components/events/SingleEvent.js
@@ -1,18 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 import classes from "./SingleEvent.module.css";
 
 export default function SingleEvent(props) {
     const {id, title, description, image, date, location} = props.singleEvent;
-    // const eventImage = image;
   return (
     <div className={classes.singleEventBody}>
       <div className={classes.singleEventImage}>
       <Image src={image} alt="event" height={366} width={500}/>
       </div>
       <div className={classes.singleEventInfo}>
-        <h5>{title}</h5>
+        <h3 className={classes.eventTitle}>{title}</h3>
         <time>{date}</time>
-        <p>{location}</p>
+        <p className={classes.eventLocation}>{location}</p>
+        <Link href={`/events/${id}`}><button className={classes.button}>Event Details</button></Link>
       </div>
     </div>
   );
